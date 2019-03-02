409983f1b185f217da6220332fed4ec6332d34bb
Basabdatta Sen Bhattacharya <basab.sen.b@gmail.com>
2019-03-02 14:32:17 +0530

    Update preproc.m

--- preproc.m
+++ preproc.m
@@ -1,3 +1,7 @@
+%% Code used in Thesis: Information Recovery from Rank-order encoded Images.
+% Basabdatta Sen Bhattacharya, Supervisor: Prof Steve Furber
+% School of Computer Science, University of Manchester 2008
+
 function N = preproc(A)
 %FUNCTION TO NORMALIZE A PICTURE USING MEAN AND STANDARD DEVIATION
 
