f85bd51aa6994b98756c7dbe4b499d24530d0827
Basabdatta Sen Bhattacharya <basab.sen.b@gmail.com>
2019-03-02 14:31:17 +0530

    Update preproc.m

--- preproc.m
+++ preproc.m
@@ -6,14 +6,3 @@ reqstd = 0.15;
 meanim = mean(A(:));
 stdim = std(A(:));
 N = (((A-meanim) ./ stdim) .* reqstd) + reqmean;
-
-
-
-% I1 = A3;
-% J1 = R3;
-% A4 = (A - meana) ./meana;
-% R4 = (R - meanr)./meanr;
-% reconwocoeff1
-% I1 = A4;
-% J1 = R4;
-% expsobelmod
